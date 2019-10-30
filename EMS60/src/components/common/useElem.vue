671a9875c1d26ef77ce9d84582685f25ca22f26e
lixiaodong <961510699@qq.com>
2019-10-30 21:16:04 +0800

    第三次提交 开始布局登录页面

---
+++ EMS60/src/components/common/useElem.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div>
+    <el-button type="success">成功按钮</el-button>
+      <h1>来了</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ EMS60/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import UserElem from '@/components/common/useElem.vue'
+import Login from '@/components/login/Login.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      name: 'userElem',
+      path: '/ele',
+      component: UserElem
+    },
+    {
+      name:'login',
+      path:'/login',
+      component:Login
+    }
+  ]
+})
